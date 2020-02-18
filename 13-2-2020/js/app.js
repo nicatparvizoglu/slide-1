87c21fb0f94967c509cfdc4d6035bc4e1e9095f1
Nicat <50620241+nicatparvizoglu@users.noreply.github.com>
2020-02-18 20:07:34 +0400

    Add files via upload

---
+++ 13-2-2020/js/app.js
@@ -0,0 +1,16 @@
+$(".mySlider").slick({
+    slidesToShow:3,
+    slidesToScroll:2,
+     centerPadding:"10px",
+     dots:true
+})
+
+// $(".mySlider").slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     centerMode: true,
+//     variableWidth: true
+//   })
+          
